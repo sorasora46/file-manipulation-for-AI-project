c76d60390d7347bc16a10a1831939d2eb8d5fbc0
sorasora46 <sorrawit02546@gmail.com>
2023-11-13 15:45:05 +0700

    some modify

--- list_high_species.py
+++ list_high_species.py
@@ -30,23 +30,27 @@ high_img_count = 0
 for key in sorted_species_file_count:
     value = sorted_species_file_count[key]
 
-    if value >= 200:
+    if value >= 230 and value <= 600:
+    # if value >= 200:
         species_dir = os.path.join(species_path, key)
-        print(species_dir)
+        # print(species_dir)
 
         target_species_path = os.path.join(targets_path, key)
         os.makedirs(target_species_path, exist_ok=True)
 
         high_img_count += 1
-        print(key, value)
+        # print(key, value)
+        print(key)
 
+        # !!! FILE OPERATION !!!
         # write to txt file
         # f.write(key + '\n')
 
         for dir, _, file in os.walk(species_dir):
             for img in file:
                 image_path = os.path.join(dir, img)
-                shutil.copy(image_path, target_species_path)
+                # !!! FILE OPERATION !!!
+                # shutil.copy(image_path, target_species_path)
 # f.close()
 
 print('count: ', high_img_count)
